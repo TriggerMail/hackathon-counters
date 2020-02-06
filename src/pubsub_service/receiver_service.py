e4177154c943719cead2b419073f7f752e85f077
Sunny Shapir <sshapir@bluecore.com>
2020-02-06 14:45:18 -0500

    path

--- src/pubsub_service/receiver_service.py
+++ src/pubsub_service/receiver_service.py
@@ -1,20 +1,18 @@
+#autoformat
 from google.cloud import pubsub_v1
-
-# TODO project_id = "Your Google Cloud Project ID"
-# TODO subscription_name = "Your Pub/Sub subscription name"
-# TODO timeout = 5.0  # "How long the subscriber should listen for
-# messages in seconds"
 
 subscriber = pubsub_v1.SubscriberClient()
 # The `subscription_path` method creates a fully qualified identifier
 # in the form `projects/{project_id}/subscriptions/{subscription_name}`
 subscription_path = subscriber.subscription_path(
-    project_id, subscription_name
+    "bluecore-qa", "hackathon"
 )
+
 
 def callback(message):
     print("Received message: {}".format(message))
     message.ack()
+
 
 streaming_pull_future = subscriber.subscribe(
     subscription_path, callback=callback
@@ -24,6 +22,6 @@ print("Listening for messages on {}..\n".format(subscription_path))
 # result() in a future will block indefinitely if `timeout` is not set,
 # unless an exception is encountered first.
 try:
-    streaming_pull_future.result(timeout=timeout)
+    streaming_pull_future.result(timeout=60)
 except:  # noqa
     streaming_pull_future.cancel()
